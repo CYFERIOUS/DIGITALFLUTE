cc9d91b6c95d16d7ed0456936b0d656bbc327a06
CYFERIOUS <frankdrujes@gmail.com>
2022-01-21 23:47:16 -0500

    3dsession done without responsive

--- sessions/3d/inD3x.php
+++ sessions/3d/inD3x.php
@@ -4,8 +4,12 @@
 <meta charset="utf-8">
 <title>3D PORTFOLIO</title>
 
-<script src="../../bower_components/jquery/dist/jquery.min.js" type="text/javascript"></script>
-<script src="../../bower_components/jquery-ui/jquery-ui.min.js" type="text/javascript"></script>
+<!--<script src="../../bower_components/jquery/dist/jquery.min.js" type="text/javascript"></script>
+<script src="../../bower_components/jquery-ui/jquery-ui.min.js" type="text/javascript"></script>-->
+
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
+
 
 <script src="js/filter.js" type="text/javascript"></script>
 	<script src="js/scriptos.js" type="text/javascript"></script>
@@ -19,28 +23,13 @@
 
 <div id="frame">
  
-	<div alt="img"><iframe id="main" src="" width="500" height="500" ></iframe> </div>
-  
-    <table id="frame-arrows">
-	
-    <tr>
-		<td id="left">
-        	<img src="images/flechas2.png" alt="left"/>
-        </td>
-        <td><div id="description">
-    <p>  </p>
-    </div></td>
-		<td id="right">
-        	<img src="images/flechas.png" alt="right"/>
-        </td>
-	</tr>
-   
-</table>
+	<div alt="img"><iframe id="main" src="" width="100%" height="400px" ></iframe> </div>
 
 </div>
 
 <div id="wraper">
 <ul id="filter">
+	3D MODELS 
 	<li class="active">ALL</li>
 	<li>VIDEO GAMES</li>
 	<li>ARCHITECTURE</li>
